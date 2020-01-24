a1ff1138cfce1fba891928fb43a7978faf2ce698
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-24 19:41:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,14 +5,13 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+    @student = Student.find(params)
   end
   def new
   end
 
   def create
       student = Student.create(first_name: params[:student][:first_name],last_name: params[:student][:last_name])
-
       redirect_to student_path(student)
   end
 end
